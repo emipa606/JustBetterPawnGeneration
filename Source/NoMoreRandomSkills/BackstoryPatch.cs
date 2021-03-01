0f49670f865d43960625e617976d96abbff2f618
emipa606 <emil.offermo@litet.net>
2021-03-01 22:56:30 +0100

    Rework for 1.2

---
+++ Source/NoMoreRandomSkills/BackstoryPatch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+
+namespace NoMoreRandomSkills
+{
+    // Token: 0x02000002 RID: 2
+    [HarmonyPatch(typeof(BackstoryDatabase), "ReloadAllBackstories")]
+    public static class BackstoryPatch
+    {
+        // Token: 0x06000002 RID: 2 RVA: 0x00002078 File Offset: 0x00000278
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instrs)
+        {
+            instrs = instrs.MethodReplacer(typeof(Backstory).GetMethod("ResolveReferences"),
+                typeof(BackstoryPatch).GetMethod("RemoveMissingSkills"));
+            return instrs;
+        }
+
+        public static void RemoveMissingSkills(this Backstory backstory)
+        {
+            backstory.ResolveReferences();
+            if (backstory.forcedTraits != null)
+            {
+                backstory.forcedTraits =
+                    PawnBioPatch.CleanUpTraitList(backstory.forcedTraits, $"Backstory: {backstory.title}");
+            }
+        }
+    }
+}
--- Source/NoMoreRandomSkills/SkillsPatch.cs
+++ Source/NoMoreRandomSkills/SkillsPatch.cs
@@ -1,72 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Reflection;
 using HarmonyLib;
-using RimWorld;
 using Verse;
 
 namespace NoMoreRandomSkills
 {
-
-    // Token: 0x02000002 RID: 2
-    [StaticConstructorOnStartup]
-    internal class NoMoreRandomSkills : Mod
+    public class HarmonyRunner : Mod
     {
-        // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
-        public NoMoreRandomSkills(ModContentPack content) : base(content)
+        public HarmonyRunner(ModContentPack content) : base(content)
         {
             var harmonyInstance = new Harmony("com.nomorerandomskills.patch");
             harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
         }
     }
 
-    [HarmonyPatch(typeof(TraitDef), "DataAtDegree", new Type[] { typeof(int) })]
-    public static class DataAtDegreePatch
-    {
-        static bool Prefix(ref TraitDegreeData __result, ref TraitDef __instance, ref int degree)
-        {
-            __result = __instance.degreeDatas[0];
-            return false;
-        }
-    }
-
-    [HarmonyPatch(typeof(Backstory), "ConfigErrors", new Type[] { typeof(bool) })]
-    public static class ConfigErrorsPatch
-    {
-        static IEnumerable<string> Postfix(IEnumerable<string> values)
-        {
-            var returnValues = new List<string>();
-            foreach (var value in values)
-            {
-                switch (value)
-                {
-                    case string a when a.Contains("has invalid trait Beauty degree="):
-                    case string b when b.Contains("has invalid trait NaturalMood degree="):
-                    case string c when c.Contains("has invalid trait Industriousness degree="):
-                    case string d when d.Contains("has invalid trait Nerves degree="):
-                    case string e when e.Contains("has invalid trait SpeedOffset degree="):
-                    case string f when f.Contains("has invalid trait PsychicSensitivity degree="):
-                        if (Prefs.DevMode) Log.Message($"Skipping error: {value} since we cannot change hardcoded backstories.");
-                        break;
-                    default:
-                        returnValues.Add(value);
-                        break;
-                }
-            }
-            return returnValues;
-        }
-    }
-    
     // Token: 0x02000002 RID: 2
-    [HarmonyPatch(typeof(PawnGenerator), "FinalLevelOfSkill", new Type[] { typeof(Pawn), typeof(SkillDef) })]
-    public static class FinalLevelOfSkillPatch
+    [HarmonyPatch(typeof(PawnGenerator), "FinalLevelOfSkill", null)]
+    public static class SkillsPatch
     {
         // Token: 0x06000002 RID: 2 RVA: 0x00002078 File Offset: 0x00000278
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instrs)
         {
-            instrs = Transpilers.MethodReplacer(instrs, typeof(Rand).GetMethod("RangeInclusive"), typeof(FinalLevelOfSkillPatch).GetMethod("SkillRangePatch"));
-            instrs = Transpilers.MethodReplacer(instrs, typeof(Rand).GetMethod("ByCurve", new Type[] { typeof(SimpleCurve) }), typeof(FinalLevelOfSkillPatch).GetMethod("SkillRangePatch2"));
-            instrs = Transpilers.MethodReplacer(instrs, typeof(Rand).GetMethod("Range", new Type[] { typeof(float), typeof(float) }), typeof(FinalLevelOfSkillPatch).GetMethod("SkillRangePatch3"));
+            instrs = instrs.MethodReplacer(typeof(Rand).GetMethod("RangeInclusive"),
+                typeof(SkillsPatch).GetMethod("SkillRangePatch"));
+            instrs = instrs.MethodReplacer(typeof(Rand).GetMethod("ByCurve"),
+                typeof(SkillsPatch).GetMethod("SkillRangePatch2"));
+            instrs = instrs.MethodReplacer(typeof(Rand).GetMethod("Range", new[]
+            {
+                typeof(float),
+                typeof(float)
+            }), typeof(SkillsPatch).GetMethod("SkillRangePatch3"));
             return instrs;
         }
 
